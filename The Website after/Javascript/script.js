f34ae3c9ab883731172b7a52d70df5b2a311a033
Hassan-Mohamed11 <asso.mohamed2008@gmail.com>
2024-12-21 17:55:47 +0200

    Add files via upload

---
+++ The Website after/Javascript/script.js
@@ -0,0 +1,40 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+})
+
+let image = document.getElementsByClassName("slide")
+let dot = document.getElementsByTagName("span")
+
+function x(y) {
+    currentSlide = 0
+    change(y)
+}
+
+currentSlide = 0
+
+change(currentSlide)
+
+function change(x) {
+
+    currentSlide += x
+
+    if (currentSlide > 2) {
+        currentSlide = 0
+    }
+    if (currentSlide < 0) {
+        currentSlide = 2
+    }
+
+    for (i = 0; i < image.length; i++) {
+        image[i].style.display = "none"
+    }
+    image[currentSlide].style.display = "block"
+
+    for (let i = 0; i < dot.length; i++){
+        dot[i].className = dot[i].className.replace("active", "")
+    }
+    dot[currentSlide].className = "active";
+}
